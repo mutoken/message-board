c097745a7c90d83645d852fb86adefe39c2016f6
Kentaro Muto <kentaro.muto@gmail.com>
2016-05-20 11:23:19 +0000

    Gemfile update for Heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,28 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
+  def edit
+  end
+  
+  def destroy
+   @message.destroy
+   redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+  
   def index
     @message = Message.new
     ##Messagesを全て取得する
     @messages = Message.all
+  end
+
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
   end
    
   def create
@@ -17,10 +37,14 @@ class MessagesController < ApplicationController
   end
   
   private
+
   def message_params
     params.require(:message).permit(:name,:body)
     ## ここまで
   end
-  
+
+  def set_message
+    @message = Message.find(params[:id])
+  end  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'messages#index'
-#  get 'messages/index'
-  resources :messages, only: [:create]
+  resources :messages, except: [:index, :new]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
